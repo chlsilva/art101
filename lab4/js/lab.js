9faef1e0969081bd025abe6d6ff2400a8db25dc6
chlsilva <chlsilva@ucsc.edu>
2024-10-17 16:23:25 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,11 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 4 hangman game
+// Author: chloe silva
+// Date: 10/07/24
 
 // Constants
 
 // Functions
 
+//Pseudocode a simple computer game
+						// initialize the game setup
+						// choose random word 
+						// check letters ued and show the word using _ _ _
+						// loop as long as player has lives left
+							// display lives remaining
+						// ask the current player for another letter
+							// checkn if letter has been used
+								// add letter to guessed list
+							// else:
+								// reveal letter on the _ _ _
+						// player win or lose and print message
+
+
+            
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
