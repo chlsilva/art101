32dce04ea6b9e2f18411a87aef7ca846ec433a20
chlsilva <chlsilva@ucsc.edu>
2024-10-21 14:24:52 -0700

    lab 5!

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,8 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 - Data Types and Variables
+// Author: chloe silva
+// Date: oct 21 2024
 
 // Constants
+
+//define variables 
+make = "Chevrolet";
+model = "Volt";
+color = "Red";
+year = 2013;
+
+//calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year<br>");
+
+
 
 // Functions
 
