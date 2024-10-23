9e24d91a8e66908bb59c0d132b68b4d0e5117b0e
chlsilva <chlsilva@ucsc.edu>
2024-10-22 17:25:14 -0700

    lab 6 :)

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,8 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: chloe silva
+// Date: oct 22 2024
 
-// Constants
+//define varables
+myTransport = ["Chevy Volt", " bus", " rides from friends", " and walking."];
+
+//create an object for my ride
+myMainRide = {
+  make:"Chevy",
+  madel: "Volt",
+  color: "red",
+  year: 2013,
+
+  age: function() {
+    return 2024 - this.year;
+  }
+}
+
+// output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
 
 // Functions
 
