08acc881a6c4142b81a84be89b2378cfa5da70c4
chlsilva <chlsilva@ucsc.edu>
2024-11-11 16:02:02 -0800

    lab11 dones

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,10 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
+// index.js - lab11 JavaScript Events and Forms
+// Author: chloe silva
+// Requirements: jQuery must be loaded for this script to work.
+// Date: nov 11 2024
 // Constants
 
 // Functions
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+// click listener for button
+$("#submit").click(function() {
+  
+  // get value of input field
+  const userName = $("#user-name").val();
+  
+  // now let's sort it
+  const userNameSorted = sortString(userName);
+  
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
